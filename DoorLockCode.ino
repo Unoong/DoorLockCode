80cb4e4131da8496897cc695cd5aa10c394b1362
leejooyub <leejooyub1@daum.net>
2016-11-29 23:16:25 +0900

    add password xor

--- DoorLockCode.ino
+++ DoorLockCode.ino
@@ -1,6 +1,11 @@
+#include<Time.h>
+#include<TimeLib.h>
+#include<Keypad.h>
+
+
 #define W0 3        //입력 단자 설정
 #define W1 4
-#define W2 5
+#define W2 2
 #define DoorP 5
 #define DoorE 6
 
@@ -37,15 +42,16 @@ static unsigned long int cards[]=
   7142241,
   7142252
                 };  // 변수를 static 으로 정의하여 유효범위를 넘어가지 않아도 소멸하지 않도록 함 
-char bits[80];
+char bits[80]={0};
 int bitcnt=0;         // bitcnt 초기화
 unsigned long long bitw=0;  // unsigned long long ==> 8byte(64bit) 범위
 unsigned int timeout=1000;  
 boolean valid = true;  //boolean 으로 True/False 판정 doorlock 
 
 char temp;
-int password;
-int password_input;
+int password=0;
+int password_input=0;
+int key_security = 141312;
 
 void setup()  // 보드의 단자에 대하서 input output 정의 및 시리얼 모니터 사용 결정
 {
@@ -62,13 +68,13 @@ void setup()  // 보드의 단자에 대하서 input output 정의 및 시리얼
   // '1'과 '0'을 나타내는 2개의 wire를 통해 1과 0 bit를 읽는 pulse를 출력한다.
   // pulse를 세는 것이 필요하다.
 
+  makepassword(); //초기비밀번호 설정
+
   attachInterrupt((W0), W0ISR, FALLING);  //   W0 단자 (여기서는 3번) 에서 입력값이 High 에서 Low 로 바뀔때  인터럽트(W0ISR) 발생시킴
                                           // 문법: attachInterrut(unit8_t pin, void(*function)(void), int mode)
   attachInterrupt((W1), W1ISR, FALLING);  //  마찬가지로 W1 단자에서 인터럽트 발생.
   attachInterrupt((W2), W2ISR, RISING);
-  Serial.println("Hello World!");       
-  
-  for(int i=0; i<sizeof(bits); i++) bits[i]=0;
+        
 }
 
 
@@ -135,6 +141,30 @@ void loop()
 
 }
 
+const byte rows = 4;
+const byte cols = 4;
+// 키패드의 행, 열의 갯수
+
+char keys[rows][cols] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+// 키패드 버튼 위치 설정
+
+byte rowPins[rows] = {8, 7, 6, 5};
+byte colPins[cols] = {4, 3, 2, 1};
+// 키패드에 연결된 핀번호 설정(데이터 시트 참고)
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
+// 키패드 오브젝트 생성
+
+int redPin = 13;
+int greenPin = 12;
+// LED 2개의 핀번호 설정
+
+
 void W0ISR(){  // W0단자에 인터럽트가 들어왔을 경우.
   bitw = (bitw<<1) | 0x0; // bitw 를 왼쪽으로 1 bit shift 시키고 0과 OR 연산 수행 --> bitw값 LSB에 0을 추가시킨다.
   bitcnt++;               // bit count 증가시킴
@@ -150,10 +180,56 @@ void W1ISR(){   // W1단자에 인터럽트가 들어왔을 경우
 
 void W2ISR() {
   matchpassword();
-  goto done;
+  goto done2;
+  
+  done2:
+   bitcnt=0;
+   bitw = 0;
+   password_input = 0;
+   valid = true;
 }
 
-void matchpassword() {
+void makepassword() {
+  while(1) {
+  temp = keypad.getKey();
+  if(temp == '1') {
+      password = password*10+1;  
+    }
+    if(temp == '2') {
+      password = password*10+2;
+    }
+    if(temp == '3') {
+      password = password*10+3;
+    }
+    if(temp == '4') {
+      password = password*10+4;
+    }
+    if(temp == '5') {
+      password = password*10+5;
+    }
+    if(temp == '6') {
+     password = password*10+6;
+    }
+    if(temp == '7') {
+      password = password*10+7;
+    }
+    if(temp == '8') {
+      password = password*10+8;
+    }
+    if(temp == '9') {
+      password = password*10+9;
+    }
+    if(temp == '0') {
+      password = password*10+0;
+    }
+    if(temp == '*' || temp == '#') {
+      password = password ^ key_security;
+      break;
+    }
+  }
+}
+void matchpassword()
+{
   while(1) {
     temp = keypad.getKey();
     if(temp == '1') {
@@ -189,11 +265,12 @@ void matchpassword() {
     if(temp == '*' || temp == '#') {
       break;
     }
-    if(password == password_input) {
+    if(password_input == (password ^ key_security)) {
       digitalWrite(DoorP, HIGH);            // DoorLock 이 열린다.
       delay(3000);
-      goto done;
     }
+  }
+  
 }
 
 
---
+++ team_init/DoorLockCode_init.ino
@@ -0,0 +1,174 @@
+#include <Time.h>
+#include <TimeLib.h>
+#include <Keypad.h>  // Keypad.h 라이브러리를 호출
+int i=0;
+int password = 0;
+char secretCode[7];  // 비밀번호 설정
+int position = 0; 
+int wrong = 0;   //틀리 회수
+int ft=10,lt=20;  // 제한할 시간
+int key = 1234567 ;
+int password_origin = 0;
+int bitw=0;
+// 비밀번호 비교시 쓸 변수 선언(맞는 경우와 틀린경우 2가지)
+
+boolean valid = true;
+const byte rows = 4;
+const byte cols = 4;
+// 키패드의 행, 열의 갯수
+
+char keys[rows][cols] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+// 키패드 버튼 위치 설정
+
+byte rowPins[rows] = {8, 7, 6, 5};
+byte colPins[cols] = {4, 3, 2, 1};
+// 키패드에 연결된 핀번호 설정(데이터 시트 참고)
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
+// 키패드 오브젝트 생성
+
+int redPin = 13;
+int greenPin = 12;
+int DoorE = 6 ;
+// LED 2개의 핀번호 설정
+
+void setup(){
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(DoorE,OUTPUT); 
+  digitalWrite(DoorE, LOW); 
+  setLocked(true);
+  Serial.begin(9600);
+    
+  setTime(23,59,0,26,11,16);  //현재 시간으로 세팅
+ }
+
+
+void loop(){
+    boolean ep,op;      
+    unsigned int      site;
+    unsigned long int card; //4byte 범위
+  
+  
+  if((key >= '0' && key <= '9') || (key >= 'A' && key <='D')
+    || (key == '*' || key == '#'))
+    {
+  // 키패드에서 입력된 값을 확인, 맞게 입력된 값일 경우(키패드에 있는 버튼이 맞을경우) 비교
+      if(key == '*')
+      { // * 버튼을 눌렀을 경우 초기화
+      position = 0; 
+      wrong = 0; 
+      setLocked(true); // 잠금상태로 세팅
+    } 
+      else if (key == '+')
+ {    
+      int ft = atoi(keypad.getKey());
+      int lt = atoi(keypad.getKey());
+  
+ if (ft>lt) 
+ {    if(hour()>=ft || hour()<=lt)
+      {blink();
+      setLocked(true);}
+      else setLocked(false);
+  } 
+ else if (hour()>=ft && hour()<lt) {      //현재 시간이 제한된 시간안에 속하면 
+       blink(); // R LED 를 켜주고 
+       setLocked(true) ;  }  // Lock 해줌
+      
+      else setLocked(false);
+   
+ 
+  }   // + 일때 함수 구현
+
+    
+
+
+   else{
+    
+    for(i=0; i<7 ; i++)
+  { secretCode[i] = keypad.getKey(); } // 비밀번호를 만들어 저장
+
+//  키를 encrypt 하는 프로그램 작성
+
+
+     if(key == secretCode[position])
+    { // 해당 자리에 맞는 비밀번호가 입력됬을 경우
+      position++; // 다음 자리로 넘어 감
+      wrong = 0; // 비밀번호 오류 값을 0으로 만듬
+    }
+    
+    else if(key != secretCode[position])
+    { // 해당 자리에 맞지 않는 비밀번호가 입력됬을 경우
+      position = 0; // 비밀번호를 맞았을 경우를 0으로 만듬
+    
+      wrong++; // 비밀번호 오류 값을 늘려준다
+   }
+  
+    if(position == 4){ // 4자리 비밀번호가 모두 맞았을 경우
+      setLocked(false); // 잠금상태를 해제 함
+      digitalWrite(DoorE, HIGH);  // 문이 열림
+    }
+    
+    if(wrong == 4){ // 비밀번호 오류를 4번 했을 경우
+      blink(); // Red LED를 깜빡여 준다.
+   
+    }
+
+bitw = atoi(secretCode);
+ site = (bitw>>25) & 0x7f;   // 25bit 오른쪽으로 옮겨 01111111 과 비트연산 => 왜 하는지 모르겠음
+    card = (bitw>>1)  & 0xffffff;   //1111111111111111
+    op = (bitw>>0)  & 0x1;    // xxxx xxxx xxxx & 0001
+    ep = (bitw>>32) & 0x1;    // False
+    if (parity(site) != ep) valid=false;      
+    if (parity(card) == op) valid=false;      
+  
+  }
+   
+  }
+  
+}
+
+
+    
+   
+   
+
+void setLocked(int locked){ // 잠금시와 해제시에 맞는 LED를 세팅해 주는 함수
+  if(locked) { // 잠금시 Red LED를 켜주고, Green LED를 꺼준다.
+  digitalWrite(redPin, HIGH);
+  digitalWrite(greenPin, LOW); 
+  }
+
+  else{ // 해제시 Red LED를 꺼주고, Green LED를 켜준다.
+    digitalWrite(redPin, LOW);
+    digitalWrite(greenPin, HIGH);
+  } 
+}
+
+void blink(){ // Red LED를 깜빡여 주는 함수 , 4번 오류 또는 제한시간에 사용되면 도어락이 잠기는 신호로 사용됨
+  for(int i = 0; i < 8; i++){ // 딜레이 만큼 Red LED를 껐다 켰다 해줌. 총 8회
+    digitalWrite(redPin, LOW);
+    delay(500);
+    digitalWrite(redPin, HIGH);
+    delay(500);
+  }
+}
+
+void encrypted()
+{}
+
+
+int parity(unsigned long int x)
+{    unsigned long int y;
+   y = x ^ (x >> 1);  // ^ : bit XOR  
+   y = y ^ (y >> 2);    
+   y = y ^ (y >> 4);
+   y = y ^ (y >> 8);
+   y = y ^ (y >>16);
+   return y & 1;    // & : bit AND
+  }  
